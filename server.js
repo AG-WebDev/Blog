0535017af9391c9f8d489d621e6cfd50904d3591
Ali Gorji <ag.aligorji@gmail.com>
2022-05-06 18:27:19 +0430

    connect app to mongodb atlas

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
+require('dotenv').config();
 const app = require("./app");
 const mongoose = require("mongoose");
 
+const uri =
+  `mongodb+srv://${process.env.MONGODB_ATLAS_USERNAME}:${process.env.MONGODB_ATLAS_PASSWORD}@cluster0.lt3j4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 mongoose
-  .connect('mongodb://localhost:27017/blog', {
+  .connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   })
   .then(() => console.log("DB connection successful!"));
 
-const port = process.env.Port || 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`App running on ${port}...`);
 });
